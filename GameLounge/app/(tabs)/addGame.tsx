32cfeb28083e6d5ed6e722ebeb5577acb9a800a5
RobiBlocks <142887221+RobiBlocks@users.noreply.github.com>
2025-09-12 17:24:05 +0200

    added "Add New Game" form

--- GameLounge/app/(tabs)/addGame.tsx
+++ GameLounge/app/(tabs)/addGame.tsx
@@ -1,23 +1,159 @@
-import { StyleSheet, Text, View } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import LogoViewer from "@/components/LogoViewer";
+import PhotoButton from "@/components/PhotoButton";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function AddGameScreen() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+  const handleSubmit = () => {
+    console.log("Game added!");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Add a New Game</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+
+        <TextInput
+          style={styles.inputTitle}
+          placeholder="Enter game title..."
+          placeholderTextColor="white"
+        />
+
+        <View style={styles.box}>
+          <ImageViewer imgSource={selectedImage || require("../../assets/images/landscape_placeholder.svg")} />
+          <PhotoButton buttonText="Choose an image" onPress={pickImageAsync} />
+        </View>
+
+        <View style={styles.box}>
+          <LogoViewer imgSource={require("../../assets/images/nintendo_Switch_2_logo.png")} />
+          <LogoViewer imgSource={require("../../assets/images/playstation_logo.jpg")} />
+          <LogoViewer imgSource={require("../../assets/images/xbox_logo.png")} />
+        </View>
+
+        <View style={styles.boxInput}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter year released..."
+            placeholderTextColor="white"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter genre..."
+            placeholderTextColor="white"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter publisher..."
+            placeholderTextColor="white"
+          />
+        </View>
+
+        <View style={styles.box}>
+          <Button buttonText="bad" />
+          <Button buttonText="mid" />
+          <Button buttonText="good" />
+          <Button buttonText="excellent" />
+          <Button buttonText="perfect" />
+        </View>
+
+        <Pressable style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Add Game</Text>
+        </Pressable>
+
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 30,
+    paddingBottom: 50,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "green",
+    backgroundColor: "#7BB4E3",
   },
-  text: {
+  box: {
+    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    backgroundColor: "#4E97D1",
+    borderColor: "#0088FF",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 20,
+    margin: 15,
+    width: "80%",
+  },
+  boxInput: {
+    alignItems: "center",
+    backgroundColor: "#4E97D1",
+    borderColor: "#0088FF",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 20,
+    margin: 15,
+    width: "80%",
+  },
+  title: {
     color: "white",
     fontWeight: "bold",
-    fontSize: 20,
-  }
+    marginBottom: 30,
+    fontSize: 30,
+  },
+  input: {
+    height: 40,
+    backgroundColor: "grey",
+    borderColor: "darkgrey",
+    borderWidth: 2,
+    borderRadius: 10,
+    marginTop: 12,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    width: "95%",
+  },
+  inputTitle: {
+    height: 60,
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    fontSize: 24,
+    width: "80%",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#0C2869",
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
 });
---
+++ GameLounge/components/ImageViewer.tsx
@@ -0,0 +1,21 @@
+import { Image } from "expo-image";
+import { StyleSheet } from "react-native";
+
+type Props = {
+  imgSource: string;
+};
+
+export default function ImageViewer({ imgSource }: Props) {
+  return (
+    <Image source={imgSource} style={styles.image} />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    margin: 30,
+    borderRadius: 10,
+  },
+});
